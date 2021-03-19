f0f4eda6bb1e7861158dc04e011a071cf542cbf9
Manuel Cabral <cabral.manuel@yandex.com>
2021-03-19 17:06:48 -0300

    main window with fixed size

--- TaskManagerDialog.py
+++ TaskManagerDialog.py
@@ -5,6 +5,7 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(406, 300)
+        MainWindow.setFixedSize(406, 300)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.tableWidget = QtWidgets.QTableWidget(self.centralwidget)
